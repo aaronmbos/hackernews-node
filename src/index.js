c1a687be6a70ffe46bdfbe238f35273fa2b9c663
Aaron Bos <aaronmbos@gmail.com>
2022-11-03 20:02:26 -0500

    Finish simple query step

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ import { ApolloServer } from "apollo-server";
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String
   }
 `;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
